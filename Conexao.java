01d5ae9a06521007dd2d0b783117ea29cc5d7897
edmar <edmaraugusto35@gmail.com>
2020-10-23 21:28:59 -0300

    primeira versao

---
+++ Conexao.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.fiemg.conexao;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
+
+
+public class Conexao {
+    public static Connection conectar(){
+        Connection con = null;
+        final String URL = "jdbc:mysql://localhost:3306/fiemg?useTimezone=true&serverTimezone=UTC";
+        final String USER = "root";
+        final String PASS = "!@#Abc123";
+        
+        try {
+            con = DriverManager.getConnection(URL, USER, PASS);
+            return con;
+        } catch (Exception e) {
+            return null;
+        }
+    }
+  public static void desconectar(Connection con) {
+        try {
+            con.close();
+        } catch (Exception e) {
+            JOptionPane.showInternalMessageDialog(null, e);
+        }
+    }
+    
+    
+}
+
+
